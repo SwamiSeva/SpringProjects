c31001b4cf2b3e262f51daecd85d94cd208abac1
SwamiSeva <shraddha.sahane23@gmail.com>
2022-10-04 18:55:22 +0530

    Adding Bean ScopeDemo Project

---
+++ ScopeDemo/src/main/java/com/soul/demo/ScopeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.soul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopeDemoApplication.class, args);
+	}
+
+}
---
+++ ScopeDemo/src/test/java/com/soul/demo/ScopeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soul.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
